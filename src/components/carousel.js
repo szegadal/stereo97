b25a7bd2d34ed3d10a799199d219f577b20269d0
szegadal <sebastian.zegada@gmail.com>
2023-05-22 11:45:25 -0400

    carrusel a medias, componentes incompletos 3

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,40 +1,47 @@
-import { useState, useEffect, useMemo, useRef, useLayoutEffect, Children } from "react";
+import {
+  useState,
+  useEffect,
+  useMemo,
+  useRef,
+  useLayoutEffect,
+  Children,
+} from "react";
 
-import Image from 'next/image';
+import Image from "next/image";
 
-import styles from './carousel.module.css';
-import utilStyles from '../styles/utils.module.css';
-
+import styles from "./carousel.module.css";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Carousel({ children }) {
-
   const [startScrollLeft, setStartScrollLeft] = useState(0);
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [carousel, setCarousel] = useState(0);
 
-
   const [scrollStyle, setScrollStyle] = useState(styles.scrollBehaviorSmooth);
-  const [current, setCurrent] = useState(1)
+  const [current, setCurrent] = useState(1);
   const carouselRef = useRef(0);
   const childRef = useRef(0);
 
   // Constants
   const slideDirectionForward = true;
-  let itemsPerView = carouselRef.current.offsetWidth / childRef.current.offsetWidth
+  let itemsPerView =
+    carouselRef.current.offsetWidth / childRef.current.offsetWidth;
 
   const changeSlide = (slideDirectionForward) => {
-    carouselRef.current.style.transitionDuration = "400ms"
+    carouselRef.current.style.transitionDuration = "400ms";
     if (slideDirectionForward) {
-      carouselRef.current.scrollLeft -= carouselRef.current.offsetWidth / itemsPerView
+      carouselRef.current.scrollLeft -=
+        carouselRef.current.offsetWidth / itemsPerView;
     } else {
-      carouselRef.current.scrollLeft += carouselRef.current.offsetWidth / itemsPerView
+      carouselRef.current.scrollLeft +=
+        carouselRef.current.offsetWidth / itemsPerView;
     }
   };
 
   const handleScroll = (event) => {
-    console.log(carouselRef.current.width)
-  }
+    console.log(carouselRef.current.width);
+  };
 
   const slides = useMemo(() => {
     // Si solo es un elemento, renderizamos sin más
@@ -46,23 +53,27 @@ export default function Carousel({ children }) {
     const items = Children.map(children, (child, index) => {
       // Creamos un ref para el primer ítem. Así accederemos a su tamaño
       if (index === 0) {
-        return <li key={index} ref={childRef}>{child}</li>
+        return (
+          <li key={index} ref={childRef}>
+            {child}
+          </li>
+        );
       }
-      return <li key={index}>{child}</li>
-    })
+      return <li key={index}>{child}</li>;
+    });
 
     // Creamos los slides que irán a la izquierda del primer slide
     const lastItems = Children.map(children, (child, index) => {
       if (index > children.length - itemsPerView - 1) {
-        return <li key={index + itemsPerView}>{child}</li>
+        return <li key={index + itemsPerView}>{child}</li>;
       }
-    })
+    });
 
     const firstItems = Children.map(children, (child, index) => {
       if (index < itemsPerView) {
-        return <li key={index + children.length + itemsPerView}>{child}</li>
+        return <li key={index + children.length + itemsPerView}>{child}</li>;
       }
-    })
+    });
 
     return [
       // Renderizamos los últimos slides. Key es +index para que sea única.
@@ -70,23 +81,26 @@ export default function Carousel({ children }) {
       // Renderizamos todos los slides
       ...items,
       // Renderizamos el primer slide. Key es +2 para que no choque con el +1 del primer slide renderizado.
-      ...firstItems
+      ...firstItems,
     ];
   }, [children, itemsPerView]);
 
   // Para posicionar el primer slide a la vista
   useLayoutEffect(() => {
-    carouselRef.current.scrollLeft = carouselRef.current.offsetWidth
-    setCurrent(itemsPerView)
+    carouselRef.current.scrollLeft = carouselRef.current.offsetWidth;
+    setCurrent(itemsPerView);
   }, [itemsPerView]);
 
   useEffect(() => {
-    window.addEventListener('mouseup', () => {
-      setIsDragging(false);
-      setScrollStyle(styles.scrollBehaviorSmooth);
-    }, false);
+    window.addEventListener(
+      "mouseup",
+      () => {
+        setIsDragging(false);
+        setScrollStyle(styles.scrollBehaviorSmooth);
+      },
+      false
+    );
   }, []);
-
 
   return (
     <>
@@ -103,7 +117,8 @@ export default function Carousel({ children }) {
             id="carouselCaretLeft"
             onClick={() => {
               changeSlide(slideDirectionForward);
-            } } />
+            }}
+          />
           <Image
             src="/images/svg-icons/caret-circle-right-thin-svgrepo-com.svg"
             height={48}
@@ -112,22 +127,34 @@ export default function Carousel({ children }) {
             id="carouselCaretRight"
             onClick={() => {
               changeSlide(!slideDirectionForward);
-            } } />
+            }}
+          />
         </div>
       </div>
       <ul
         className={[styles.carousel, scrollStyle].join(" ")}
         ref={carouselRef}
         onScroll={() => {
-          if (carouselRef.current.scrollLeft < (carouselRef.current.offsetWidth / itemsPerView)*0.1) {
-            carouselRef.current.scrollLeft = carouselRef.current.offsetWidth + carouselRef.current.offsetWidth / itemsPerView
-            setScrollStyle(styles.scrollBehaviorAuto)
+          if (
+            carouselRef.current.scrollLeft <
+              (carouselRef.current.offsetWidth / itemsPerView) * 0.1 ||
+            carouselRef.current.scrollLeft >=
+              (carouselRef.current.scrollWidth -
+                carouselRef.current.offsetWidth) *
+                0.99
+          ) {
+            setScrollStyle(styles.scrollBehaviorAuto);
           }
-          if (carouselRef.current.scrollLeft >= (carouselRef.current.scrollWidth - carouselRef.current.offsetWidth)*0.99) {
-            console.log(carouselRef.current.scrollWidth)
-            console.log(carouselRef.current.offsetWidth)
-            carouselRef.current.scrollLeft = carouselRef.current.offsetWidth
-            setScrollStyle(styles.scrollBehaviorAuto)
+          if (carouselRef.current.scrollLeft === 0) {
+            carouselRef.current.scrollLeft =
+              carouselRef.current.scrollWidth -
+              2 * carouselRef.current.offsetWidth;
+          }
+          if (
+            Math.ceil(carouselRef.current.scrollLeft) ===
+            carouselRef.current.scrollWidth - carouselRef.current.offsetWidth
+          ) {
+            carouselRef.current.scrollLeft = carouselRef.current.offsetWidth;
           }
         }}
         onMouseEnter={() => setScrollStyle(styles.scrollBehaviorAuto)}
@@ -136,14 +163,15 @@ export default function Carousel({ children }) {
           if (!isDragging) return;
           carouselRef.current.scrollLeft = carousel;
           setCarousel(startScrollLeft - (event.clientX - startX));
-        } }
+        }}
         onMouseDown={(event) => {
           setIsDragging(true);
           setStartX(event.clientX);
           setStartScrollLeft(carouselRef.current.scrollLeft);
-        } }
-      >{slides}</ul>
+        }}
+      >
+        {slides}
+      </ul>
     </>
-
   );
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,19 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 
-import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
 
-import Header from './header'
-import Carousel from './carousel'
-import Card from './card'
+import Header from "./header";
+import Carousel from "./carousel";
+import Card from "./card";
 
-const siteTitle = 'Radio emisora Stereo97'
-const siteDescription = "Sintoniza Stereo97 para disfrutar de la mejor música rock nacional e internacional y las últimas tendencias en rock pop, house y electrónica. Stereo97, la número uno."
+const siteTitle = "Radio emisora Stereo97";
+const siteDescription =
+  "Sintoniza Stereo97 para disfrutar de la mejor música rock nacional e internacional y las últimas tendencias en rock pop, house y electrónica. Stereo97, la número uno.";
 const shows = [
   {
     id: 91,
@@ -21,7 +22,7 @@ const shows = [
     hourOnAir: "De 8:00 a 11:00",
     imageText: "Portada de Day by Day",
     imageUrl: "/images/day-by-day.jpg",
-    isDraggable: false
+    isDraggable: false,
   },
   {
     id: 92,
@@ -30,7 +31,7 @@ const shows = [
     hourOnAir: "De 11:00 a 15:00",
     imageText: "Portada de Stereografía",
     imageUrl: "/images/stereografia.jpg",
-    isDraggable: false
+    isDraggable: false,
   },
   {
     id: 93,
@@ -39,7 +40,7 @@ const shows = [
     hourOnAir: "De 15:00 a 18:00",
     imageText: "Portada de ShowTime",
     imageUrl: "/images/showtime.jpg",
-    isDraggable: false
+    isDraggable: false,
   },
   {
     id: 94,
@@ -48,12 +49,11 @@ const shows = [
     hourOnAir: "A las 20:00",
     imageText: "Portada de Gravedad Zero",
     imageUrl: "/images/gravedad-zero.jpeg",
-    isDraggable: false
-  }
-]
+    isDraggable: false,
+  },
+];
 
 export default function Layout({ children, home }) {
-
   return (
     <>
       {children}
@@ -65,34 +65,34 @@ export default function Layout({ children, home }) {
         {/* Nota: es una por página */}
         {/* <meta name="twitter:card" content="summary_large_image" /> */}
       </Head>
-      <main className={styles.layout}>
-        <Header home/>
-        <div className={styles.aboveThefold} id="home">
-          <div className={[styles.heroContent, utilStyles.stack].join(" ")}>
-            <hgroup className={utilStyles.stack}>
-              <h1>
-                Tus artistas favoritos <span>en un solo lugar</span>
-              </h1>
-              {/* <h2>
+      <div>
+        <Header home />
+        <main className={styles.layout}>
+          <div className={styles.aboveThefold} id="home">
+            <div className={[styles.heroContent, utilStyles.stack].join(" ")}>
+              <hgroup className={utilStyles.stack}>
+                <h1>
+                  Tus artistas favoritos <span>en un solo lugar</span>
+                </h1>
+                {/* <h2>
                 Sólo en la número 1
               </h2> */}
-            </hgroup>
+              </hgroup>
+            </div>
+            <Image
+              priority
+              src="/images/logo-transparente.png"
+              height={150}
+              width={300}
+              alt="Logo de Stereo 97"
+              // className={utilStyles.filterGrayscale}
+            />
+            {/* <div className={styles.heroCallToAction}>Sintoniza FM 97.3</div> */}
           </div>
-          <Image
-            priority
-            src="/images/logo-transparente.png"
-            height={150}
-            width={300}
-            alt="Logo de Stereo 97"
-            // className={utilStyles.filterGrayscale}
-          />
-          {/* <div className={styles.heroCallToAction}>Sintoniza FM 97.3</div> */}
 
-        </div>
-
-        <section className={[styles.section, styles.shows].join(" ")} >
+          <section className={[styles.section, styles.shows].join(" ")}>
             <Carousel>
-              <div className={[styles.whiteText,styles.card].join(" ")}>
+              <div className={[styles.whiteText, styles.card].join(" ")}>
                 <Image
                   src="/images/day-by-day.jpeg"
                   height={200}
@@ -104,9 +104,9 @@ export default function Layout({ children, home }) {
                 <p>Lunes a viernes</p>
                 <p>De 7:00 a 11:00</p>
               </div>
-            <div className={[styles.whiteText, styles.card].join(" ")}>
+              <div className={[styles.whiteText, styles.card].join(" ")}>
                 <Image
-                src="/images/stereografia.jpg"
+                  src="/images/stereografia.jpg"
                   height={200}
                   width={200}
                   alt="Portada de Stereografía"
@@ -116,9 +116,9 @@ export default function Layout({ children, home }) {
                 <p>Lunes a viernes</p>
                 <p>De 11:00 a 15:00</p>
               </div>
-            <div className={[styles.whiteText, styles.card].join(" ")}>
+              <div className={[styles.whiteText, styles.card].join(" ")}>
                 <Image
-                src="/images/showtime.jpg"
+                  src="/images/showtime.jpg"
                   height={200}
                   width={200}
                   alt="Portada de El Showtime"
@@ -128,7 +128,7 @@ export default function Layout({ children, home }) {
                 <p>Lunes a viernes</p>
                 <p>De 15:00 a 18:00</p>
               </div>
-            <div className={[styles.whiteText, styles.card].join(" ")}>
+              <div className={[styles.whiteText, styles.card].join(" ")}>
                 <Image
                   src="/images/gravedad-zero.jpeg"
                   height={200}
@@ -141,49 +141,65 @@ export default function Layout({ children, home }) {
                 <p>A las 20:00</p>
               </div>
             </Carousel>
-        </section>
+          </section>
 
-
-        <section id="programacion" className={styles.section}>
-          <div className={[styles.sectionHeader, styles.invertColor, utilStyles.cluster].join(" ")}>
-            <h2>Programación</h2>
-          </div>
-          <div className={styles.schedule}>
-            <div className={styles.scheduleItem}>
-              <div>7pm</div>
-              <div>The Ace Chart</div>
+          <section id="programacion" className={styles.section}>
+            <div
+              className={[
+                styles.sectionHeader,
+                styles.invertColor,
+                utilStyles.cluster,
+              ].join(" ")}
+            >
+              <h2>Programación</h2>
             </div>
-            <div className={styles.scheduleItem}>
-              <div>8pm</div>
-              <div>EDM</div>
+            <div className={styles.schedule}>
+              <div className={styles.scheduleItem}>
+                <div>7pm</div>
+                <div>The Ace Chart</div>
+              </div>
+              <div className={styles.scheduleItem}>
+                <div>8pm</div>
+                <div>EDM</div>
+              </div>
+              <div className={styles.scheduleItem}>
+                <div>9pm</div>
+                <div>Los 7 a las 7</div>
+              </div>
+              <div className={styles.scheduleItem}>
+                <div>10pm</div>
+                <div>Up Beat</div>
+              </div>
             </div>
-            <div className={styles.scheduleItem}>
-              <div>9pm</div>
-              <div>Los 7 a las 7</div>
-            </div>
-            <div className={styles.scheduleItem}>
-              <div>10pm</div>
-              <div>Up Beat</div>
-            </div>
-          </div>
-        </section>
-        {/* <section>
+          </section>
+          {/* <section>
           {children}
         </section> */}
-        <div className={styles.player}>
-          <div className={styles.playerCover}>
-            <iframe src="https://stream.consultoradas.com/cp/widgets/player/single/?p=8104" height="115" width="100%" frameBorder="no"></iframe>
+          <div className={styles.player}>
+            <div className={styles.playerCover}>
+              <iframe
+                src="https://stream.consultoradas.com/cp/widgets/player/single/?p=8104"
+                height="115"
+                width="100%"
+                frameBorder="no"
+              ></iframe>
+            </div>
+            <div className={styles.playerControls}>
+              <iframe
+                src="https://stream.consultoradas.com/cp/widgets/player/single/?p=8104"
+                height="115"
+                width="100%"
+                frameBorder="no"
+              ></iframe>
+            </div>
           </div>
-          <div className={styles.playerControls}>
-            <iframe src="https://stream.consultoradas.com/cp/widgets/player/single/?p=8104" height="115" width="100%" frameBorder="no"></iframe>
-          </div>
-        </div>
-        {!home && (
-          <div className={styles.backToHome}>
-            <Link href="/">← Back to home</Link>
-          </div>
-        )}
-      </main>
+          {!home && (
+            <div className={styles.backToHome}>
+              <Link href="/">← Back to home</Link>
+            </div>
+          )}
+        </main>
+      </div>
     </>
   );
 }
